3bcffd5300dec65d09b025f3752bb8b6150d5198
xardesucha <79457563+xardesucha@users.noreply.github.com>
2021-02-23 14:34:02 +0100

    pierwsze zadanka

---
+++ cw1/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+// zadanie 1
+        int A = 1;
+        double B = 2.7;
+        char C ='f';
+        String D ="tekst";
+//  zadanie 2
+       int myIntA = 4;
+       int myIntB = 7;
+       int myIntC = myIntA * myIntA + myIntB * myIntB;
+        System.out.println("Wynik = " + myIntC);
+//  zadanie 3
+        double A = 7;
+        double intB = A * A;
+        double sqrt = Math.sqrt(3);
+        System.out.println(intB * sqrt / 4);
+//  zadanie 4
+        String stringA = "Krzysztof";
+        String stringB = " Rykaczewski";
+        String stringsuma = stringA + stringB;
+        System.out.println(stringsuma);
+
+
+    }
+}
