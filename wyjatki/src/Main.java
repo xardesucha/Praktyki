c4476577a6d3252f7c269aa3a194343b7a338fb2
xardesucha <79457563+xardesucha@users.noreply.github.com>
2021-03-09 21:54:21 +0100

    Wyjątki zadanie 2

---
+++ wyjatki/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tab[]=new int[4];
+        try {
+            for (int i = 0; i < 5; i++) {
+                tab[i] = scanner.nextInt();
+
+            }
+        } catch (InputMismatchException inputmismatchexception) {
+            System.out.println("Nie podano liczb typu int");
+        }
+        catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+            System.out.println("Wpisano za dużo liczb do tablicy");
+        }
+    }
+} 
